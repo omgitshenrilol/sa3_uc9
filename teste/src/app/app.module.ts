5b1cd4eb18bff2582fd4ab69bb465d869b98270e
h <henriquesamu3l@gmaim.com>
2022-05-31 13:46:19 -0300

    atividade sa3_uc9

---
+++ teste/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContatoComponent } from './views/contato/contato.component';
+import { JogadorComponent } from './views/jogador/jogador.component';
+import { HomeComponent } from './views/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContatoComponent,
+    JogadorComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
